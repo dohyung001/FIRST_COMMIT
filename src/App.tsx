c076b2341b4759c8b7330c272b6da93a5c9770bc
dohyung <154952683+dohyung001@users.noreply.github.com>
2025-03-30 16:13:54 +0900

    feat: 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import viteLogo from '/vite.svg';
 function App() {
   const [count, setCount] = useState(0);
   const example = { name: 'John', age: 30, location: 'New York' };
-
+  console.log(example);
   return (
     <>
       <div>
@@ -22,7 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
